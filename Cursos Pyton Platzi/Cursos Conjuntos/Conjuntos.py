2c534b0b740d8fa39ed5ca2010dfdbacc22422d0
bigbaldh3ad <bigbaldheadfan2024@gmail.com>
2024-09-26 08:29:04 -0500

    Subo archivos de los cursos

---
+++ Cursos Pyton Platzi/Cursos Conjuntos/Conjuntos.py
@@ -0,0 +1,11 @@
+set_countries = {"col", "pan", "mex"}
+print (set_countries) 
+
+set_nombers = {1 , 2, 3, 4, 5, 5,}
+print (set_nombers)
+
+set_types = {4, "arroz" , 4, False, True, 45 , "panqueque"}
+print (set_types)
+
+set_from_string = set("hoola")
+print (set_from_string)

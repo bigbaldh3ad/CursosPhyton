1099c2bd98cb8bf348700324d8767060f028f093
bigbaldh3ad <bigbaldheadfan2024@gmail.com>
2024-10-05 12:56:24 -0500

    Actualizo mi Repositorio con archivos del Curso Archivos

---
+++ Cursos Pyton Platzi/Lectura y Escritura de Archivos/Manejo de Archivos .TXT.py
@@ -0,0 +1,18 @@
+#Leer un archivo linea por linea
+"""with open ('Historia.txt','r') as file:
+    for lineas in file:
+        print(lineas.strip())"""
+
+#Leer todas las lineas en una lista
+"""with open ('Historia.txt','r') as file:
+    lines = file.readline()
+    print(lines)"""
+
+#Añadir texto
+"""with open ('Historia.txt','a') as file:  
+    file.write("\n\n By:Fernando GPT") """
+    
+#sobreescribir texto
+with open ('Historia.txt','w') as file:
+       file.write("\n\n By:Fernando GPT")
+       

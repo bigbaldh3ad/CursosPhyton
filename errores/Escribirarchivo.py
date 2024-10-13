cf5609dfb3ca5aa5188ae4261f6c626aa772fa9e
bigbaldh3ad <bigbaldheadfan2024@gmail.com>
2024-10-13 09:46:51 -0500

    Eliminados todos los archivos del repositorio

--- errores/Escribirarchivo.py
+++
@@ -1,6 +0,0 @@
-with open('./texto.txt', 'w+') as file:
-  for line in file:
-    print(line)
-  file.write('nuevas cosas en este archivo\n')
-  file.write('otra linea\n')
-  file.write(' mas linea\n')
